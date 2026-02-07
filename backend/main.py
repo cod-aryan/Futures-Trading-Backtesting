e1c18a1b22364600cedf9502b2fa017530dc5890
Aryan-Git-Hub <aryan.hmt1234@gmail.com>
2026-02-07 18:23:19 +0530

    minor

--- backend/main.py
+++ backend/main.py
@@ -4,12 +4,17 @@ from fastapi.middleware.cors import CORSMiddleware
 from routes.data import router as data_router
 from routes.backtest import router as backtest_router
 from routes.trade import router as trade_router
+import os
+from dotenv import load_dotenv
 
 app = FastAPI(title="Trading Backtest API")
 
+load_dotenv()
+
+# Then modify the middleware:
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[os.getenv("CORS_ORIGIN", "http://localhost:3000")],
     allow_methods=["*"],
     allow_headers=["*"],
 )
